7e742277660bd8f8d678f1c82c72515aec423d2a
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-05-11 22:07:16 -0700

    fixed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,12 +7,12 @@ startBtn.disabled = true;
 let timerStatus = "";
 
 
-refs = {
+const refs = {
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
-}
+};
 
 
 const options = {
